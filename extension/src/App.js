660e4cb300285e9d36dcb2cadaae1c4cc043eff0
OldSinner <antylifepatia@gmail.com>
2023-07-13 19:59:23 +0200

    Nie umiem we front

--- extension/src/App.js
+++ extension/src/App.js
@@ -1,21 +1,41 @@
 import "./App.css";
-
+import loading from "./assets/loading.svg";
+import { useState } from "react";
 function App() {
+  const [status, setStatus] = useState(1);
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="header">
+          Czy <span className="hlight">Wuja</span> jest Live?
+        </div>
+        {status === 0 && (
+          <div className="content">
+            <img src={loading} alt="loading" className="loading" />
+          </div>
+        )}
+        {status === 1 && (
+          <div className="content">
+            <div className="content_text">
+              Stary nie streamuje, ale zawsze możesz obejrzeć shoty na kanale{" "}
+              <a href="https://www.youtube.com/@AwizoTV">AwizoTV</a>
+            </div>
+          </div>
+        )}
+        {status === 2 && (
+          <div className="content">
+            <div className="content_text">NIE</div>
+          </div>
+        )}
+        {status === 2 && (
+          <div className="content">
+            <div className="content_text">NIE</div>
+          </div>
+        )}
+        <div className="footer">
+          <button className="footer_button">Sprawdź!</button>
+        </div>
+      </div>
     </div>
   );
 }
